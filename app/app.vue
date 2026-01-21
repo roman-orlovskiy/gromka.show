3706ee025475d8c04f839dcc4b270c4b3725b1fb
Roman Orlovskiy <irom-io@ya.ru>
2026-01-21 13:31:55 +0300

    Fix root page

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
+  <div class="app-root">
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+html,
+body {
+  min-height: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#__nuxt {
+  min-height: 100%;
+  height: 100%;
+  width: 100%;
+}
+
+.app-root {
+  min-height: 100%;
+  height: 100%;
+  width: 100%;
+}
+</style>
