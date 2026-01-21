d791b02563787b30ef63817459339dba4cf1da6a
Roman Orlovskiy <irom-io@ya.ru>
2026-01-21 17:49:01 +0300

    Add inter font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt']
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'
+        }
+      ]
+    }
+  }
 })
