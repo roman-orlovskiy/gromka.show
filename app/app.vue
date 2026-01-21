dfaaf90121a28910ec6e4e2fee82b725d124a314
Roman Orlovskiy <irom-io@ya.ru>
2026-01-21 20:38:32 +0300

    Add rem

--- app/app.vue
+++ app/app.vue
@@ -8,7 +8,14 @@
 <style lang="scss">
 @use "@/assets/scss/fonts.scss";
 
-html,
+html {
+  font-size: clamp(0.875rem, 0.75rem + 0.5vw, 1.125rem);
+  min-height: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 body {
   min-height: 100%;
   height: 100%;
@@ -16,6 +23,8 @@ body {
   padding: 0;
   font-family: 'TT Travels Next', sans-serif;
   font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.5;
 }
 
 #__nuxt {
